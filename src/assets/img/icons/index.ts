8ee52e44fe7b821c9192c71692e1cf9eea5fbd23
clean-kim <kimlin8798@naver.com>
2024-02-06 23:39:09 +0900

    css 수정

--- src/assets/img/icons/index.ts
+++ src/assets/img/icons/index.ts
@@ -3,3 +3,4 @@ export { default as icon_heart } from './icon_heart.svg';
 export { default as icon_heart_fill } from './icon_heart_fill.svg';
 export { default as icon_add_square } from './icon_add_square.svg';
 export { default as github_logo } from './github_logo.svg';
+export { default as icon_cart } from './icon_cart.svg';
